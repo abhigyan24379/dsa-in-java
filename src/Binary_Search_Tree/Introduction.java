d67d90e13bdcc12ef8795930f5f0cb01604fe854
abhigyan24379 <abhigyan03062003@gmail.com>
2025-08-24 12:44:25 +0530

    deletion , searching

--- src/Binary_Search_Tree/Introduction.java
+++ src/Binary_Search_Tree/Introduction.java
@@ -29,7 +29,7 @@ public class Introduction {
             return ;
         }
         inorder(root.left);
-        System.out.println(root.data+" ");
+        System.out.print(root.data+" ");
         inorder(root.right);
 
     }
@@ -49,21 +49,92 @@ public class Introduction {
         }
 
     }
+    public static Node delete(Node root, int val ){
+        if(root.data < val){
+            root.right  = delete(root.right, val);
+        }
+        else if(root.data > val){
+            root.left = delete(root.left, val);
+        }
+        else{
+            // Case 1 - leaf node
+            if(root.right == null && root.left == null ){
+                return null;
+            }
+            // case 2 single child
+            if(root.right == null)
+                return root.left;
+            else if(root.left == null)
+                return root.right;
+            // case 3 both children
+            Node Is = FindInorderSuccessor(root.right);
+            root.data = Is.data;
+            root.right = delete(root.right, Is.data);
+        }
+        return root;
+    }
+    public static Node FindInorderSuccessor(Node root){
+        while(root.left != null){
+            root = root.left;
+        }
+        return root;
+    }
+
+    public static void PrintInRange(Node root , int k1, int k2){
+        if(root == null){
+            return ;
+        }
+        if(root.data >= k1 && root.data <= k2){
+            PrintInRange(root.left, k1, k2);
+            System.out.print(root.data+" ");
+            PrintInRange(root.right , k1, k2);
+        }
+        else if( root.data >= k1){
+            PrintInRange(root.right, k1, k2);
+        }
+        else{
+            PrintInRange(root.left, k1, k2);
+        }
+    }
+
+    public static boolean isValid(Node root , Node min , Node max){
+        if(root == null){
+            return true;
+        }
+        if(min!= null && root.data <= min.data){
+            return false ;
+        }
+        else if(max!= null && root.data >= max.data){
+            return false ;
+        }
+        return isValid(root.left, min , root) && isValid(root.right, root, max);
+
+    }
 
 
     public static void main(String[] args) {
-        int values[] = {1,2,3,4,5,6};
+        int values[] = {8,5,3,1,6,10,11,14};
+//        int values[] = {1,1,1};
+
         Node root = null;
         for( int i =0; i < values.length ; i++){
             root = insert(root, values[i]);
         }
         inorder((root));
+
+//        root = delete(root, 10 );
+//        System.out.println();
+//        inorder(root);
         System.out.println();
-        if(search(root, 6)){
-            System.out.println("found");
-        }else{
-            System.out.println("not found");
+        if(isValid(root, null, null)){
+            System.out.println("IsValide");
         }
-        search(root, 1);
+        else{
+            System.out.println("Not valid");
+        }
+
+
+
+
     }
 }
